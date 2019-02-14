00d3058606cea54df298dbb10b79d093ed7ebd79
Michael Cotterell (Supa' Mike) <mepcotterell@gmail.com>
2019-02-14 06:01:58 -0500

    added more test cases

--- main.c
+++ main.c
@@ -7,7 +7,11 @@ void printGCD(long a, long b, long expected) {
 } // printGCD
 
 int main() {
-  printGCD(54, 24, 6);
+  printGCD( 54, 24,  6);
+  printGCD( 24, 24, 24);
+  printGCD(256,  2,  2);
+  printGCD( 33,  2,  1);
+  printGCD( 45, 18,  9);  
   return 0;
 } // main
 
